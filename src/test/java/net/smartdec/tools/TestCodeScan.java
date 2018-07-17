4c8236dfa8611637f04322288f6b31100ccc17b1
Naumov Vladislav <vlad.naumov57@gmail.com>
2018-07-17 17:09:55 +0300

    Добавить проверки, что код проанализирован Добавить ожидание загрузки страницы с полем для вставки кода Выделить wait для ожидания загрузки страницы

--- src/test/java/net/smartdec/tools/TestCodeScan.java
+++ src/test/java/net/smartdec/tools/TestCodeScan.java
@@ -20,15 +20,18 @@ public class TestCodeScan {
     public void testScanSimpleJavaProgramWithoutErrors() {
 
         // подготовка к тесту
+        // TODO отделить метод setUp который выполняется до теста
         URL url = getClass().getClassLoader().getResource("chromedriver.exe"); /* Не утверждаю, что так правильно.
         Возможно, на реальном проекте лучше хранить драйвер, например, на общем сетевом ресурсе.
         Места он занимает немного, поэтому здесь драйвер добавлен в ресурсы проекта для удобства проверяющего. */
         System.setProperty("webdriver.chrome.driver", url.getFile());
         WebDriver wd = new ChromeDriver();
+        WebDriverWait pageLoadWait = new WebDriverWait(wd, 10);
 
         // переходим на стартовую страницу
         wd.get("http://tool.smartdec.net");
 
+        // TODO убрать дублирование кода в функцию
         // нажимаем на кнопку start using
         WebElement startUsingButton = wd.findElement(By.id("btn-start"));
         Assert.assertNotNull("Кнопка start using не найдена на странице", startUsingButton);
@@ -38,8 +41,10 @@ public class TestCodeScan {
         WebElement pasteCodeButton = wd.findElement(By.xpath("//*[@id='actions-ul']//*[text()='paste code']"));
         Assert.assertNotNull("Кнопка paste code не найдена на странице", pasteCodeButton);
         pasteCodeButton.click();
-        Assert.assertEquals("Не удалось перейти на страницу отправки кода для сканирования",
-                "http://tool.smartdec.net/newscan?type=codeblock", wd.getCurrentUrl()); // TODO добавить ожидание загрузки страницы и отделить проверку
+
+        // ожидаем загрузки и проверяем страницу для отправки кода на сканирование
+        Boolean pasteCodeButtonPressResult = pageLoadWait.until(ExpectedConditions.urlToBe("http://tool.smartdec.net/newscan?type=codeblock"));
+        Assert.assertTrue("Не удалось перейти на страницу отправки кода для сканирования", pasteCodeButtonPressResult);
 
         // вставляем код в textarea
         WebElement textArea = wd.findElement(By.id("codeblock"));
@@ -57,19 +62,23 @@ public class TestCodeScan {
         submitForScanButton.click();
 
         // ожидаем загрузки страницы
-        WebDriverWait wait = new WebDriverWait(wd, 10);
-        Boolean scanResult = wait.until(ExpectedConditions.urlMatches("http://tool.smartdec.net/scan/[0-9a-z]*"));
+        Boolean scanResult = pageLoadWait.until(ExpectedConditions.urlMatches("http://tool.smartdec.net/scan/[0-9a-z]*"));
         Assert.assertTrue("Не удалось перейти на страницу с результатами анализа кода", scanResult);
 
         // проверяем результат
 
         // 1 - проверяем, что на странице выведено No Source и No selected bug/source
-        // TODO добавить код проверки
+        WebElement scanResultHeader = wd.findElement(By.xpath("//div[@id='source-block']//*[text()[contains(.,'No Source')]]"));
+        Assert.assertNotNull("На странице не найден ожидаемый блок с текстом No source", scanResultHeader);
+        WebElement scanResultSelectedBug = wd.findElement(By.xpath("//div[@id='source-block']//*[text()[contains(.,'No selected bug')]]"));
+        Assert.assertNotNull("На странице не найден ожидаемый блок с текстом No selected bug", scanResultSelectedBug);
 
         // 2 - проверяем, что на странице доступен файл codeblock.sol с результатами
-        // TODO добавить код проверки
+        WebElement scanResultCodeBlockFile = wd.findElement(By.xpath("//span[text()='codeblock.sol']"));
+        Assert.assertNotNull("На странице не найден ожидаемый файд с результатами codeblock.sol", scanResultCodeBlockFile);
 
         // завершаем работу
+        // TODO отделить метод tearDown который выполняется после теста
         wd.quit();
     }
 }
